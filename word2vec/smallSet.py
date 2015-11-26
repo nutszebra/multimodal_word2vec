4e63b59b8e7ddef23f21e8c5a9d664c61c271dae
EC2 Default User <ec2-user@ip-172-31-11-20.ap-northeast-1.compute.internal>
2015-11-26 11:35:32 +0000

    divide cifar10

---
+++ word2vec/smallSet.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+from six.moves.urllib import request
+import cPickle as pickle
+import random
+import os
+import cv2
+import shutil
+import subprocess
+import numpy as np
+
+picBase = "/mnt/s3pic/cifar10/"
+
+def save_object(obj, filename):
+  with open(filename, 'wb') as output:
+    pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def load_object(path):
+  with open(path, 'r') as f:
+    answer = pickle.load(f)
+  return answer
+
+def checkExistance(path):
+  if os.path.exists(path):
+    return True
+  else:
+    return False
+
+def makeDirectory(path):
+  if not checkExistance(path):
+    os.makedirs(path)
+
+def downloadPic(url, name):
+  cmd = "wget " + url + " -O " + name + " -q"
+  subprocess.call(cmd, shell=True)
+
+def extractExtension(name):
+  return re.findall(r"^.*(\..*)$", name)[0]
+
+def moveFile(path, name):
+  cmd = "mv " + name + " " + path
+  subprocess.call(cmd, shell=True)
+
+def cifar100Extract():
+  makeDirectory(picBase)
+  makeDirectory(picBase + "train")
+  makeDirectory(picBase + "test")
+  makeDirectory(picBase + "label")
+  if not os.path.exists("cifar-100-python"):
+    request.urlretrieve(
+     "http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz",
+     "./cifar-100-python.tar.gz"
+    )
+    cmd = "tar -xvzf cifar-100-python.tar.gz"
+    subprocess.call(cmd, shell=True) 
+    cmd = "rm -r cifar-100-python.tar.gz"
+    subprocess.call(cmd, shell=True) 
+  def unpickle(file):
+    import cPickle
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo)
+    fo.close()
+    return dict
+  train = unpickle("cifar-100-python/train")
+  test = unpickle("cifar-100-python/test")
+  tag = unpickle("cifar-100-python/meta")
+  tagWith_ = ['aquarium_fish',
+   'lawn_mower',
+   'maple_tree',
+   'oak_tree',
+   'palm_tree',
+   'pickup_truck',
+   'pine_tree',
+   'sweet_pepper',
+   'willow_tree']
+  tagAlter = ["fish",
+    "lawnmower",
+    "maple",
+    "oak",
+    "palm",
+    "truck",
+    "pine",
+    "paprika",
+    "willow"]
+  index = [tag["fine_label_names"].index(with_) for with_ in tagWith_]
+  count = 0
+  for i in index:
+    tag["fine_label_names"][i] = tagAlter[count]
+    count = count + 1
+  y_train = {}
+  y_test = {}
+  x_test = test['data']
+  x_test = x_test.reshape(len(x_test),3,32,32)
+  x_train = train['data']
+  x_train= x_train.reshape(len(x_train),3,32,32)
+  for x in zip(x_test, test["filenames"], test["fine_labels"]):
+    cv2.imwrite(picBase + "test/" + x[1], x[0].transpose(1,2,0)[:,:,::-1].copy())
+    y_test[x[1]] = x[2]
+  for x in zip(x_train, train["filenames"], train["fine_labels"]):
+    cv2.imwrite(picBase + "train/" + x[1], x[0].transpose(1,2,0)[:,:,::-1].copy())
+    y_train[x[1]] = x[2]
+  
+  save_object(y_test, picBase + "label/y_test.pkl")
+  save_object(y_train, picBase + "label/y_train.pkl")
+  return (train, test, tag)
+
+def swapKeyAndVal(dic):
+  answer = {}
+  for key in dic:
+    if not dic[key] in answer:
+      answer[dic[key]] = []
+    answer[dic[key]].append(key)
+  return answer
+
+def swapValAndKey(dic):
+  answer = {}
+  for key in dic:
+   for i in dic[key]:
+     answer[i] = key
+  return answer
+
+def copyFile(namelist, path_from, path_to):
+  for name in namelist:
+    shutil.copy2(path_from + name, path_to + name)
+
+"""
+In [38]: tag.keys()
+Out[38]: ['fine_label_names', 'coarse_label_names']
+In [40]: train.keys()
+Out[40]: ['data', 'batch_label', 'fine_labels', 'coarse_labels', 'filenames'
+In [41]: len(train["data"])
+Out[41]: 50000
+In [42]: len(train["data"][0])
+Out[42]: 3072 // it means 32*32*3
+"""
+
+if __name__ == '__main__':
+ # x_train, y_train, x_test, y_test, tag = cifar100Extract()
+  picNum = 100
+  trainPath = picBase + "label/y_train.pkl"
+  train_y = load_object(trainPath)
+  train_y_sw = swapKeyAndVal(train_y)
+  newTrainSet = {}
+  for key in train_y_sw:
+    newTrainSet[key] = random.sample(train_y_sw[key], picNum)
+  if not checkExistance(picBase + "smallTrain_x"):
+    makeDirectory(picBase + "smallTrain_x")
+    makeDirectory(picBase + "smallTrain_y")
+    for key in newTrainSet:
+      copyFile(newTrainSet[key], picBase + "train/", picBase + "smallTrain_x/")
+    save_object(swapValAndKey(newTrainSet), picBase + "smallTrain_y/train_y.pkl")
